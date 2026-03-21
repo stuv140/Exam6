e0251fc2dc362af6dc275142a67b0f12dc66bb7b
stuv140 <stuv.otus@yandex.ru>
2026-03-21 13:05:38 +0300

    Create Proxy.h

---
+++ include/Proxy.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "matrix.h"
+#include <memory>
+
+template<class T, T N>
+class Matrix;
+
+
+template<class T, T N>
+class Proxy {
+public:
+    Proxy(Matrix<T, N>* matrixPtr_, long x_) :
+        matrixPtr(matrixPtr_),  x(x_)
+    {
+    };
+
+    ~Proxy() {
+    };
+
+    Proxy& operator[](int y) {
+        this->y = y;
+        // Запрашиваем текущее значение из матрицы по координатам (x, y)
+        // и сохраняем его в локальную переменную element.     
+        element = matrixPtr->getValueAt(x, y);
+        return *this;
+    };
+
+    void operator=(T newValue) {
+        matrixPtr->setValueAt(x, y, newValue);
+    }
+    operator T() const { //ДЛЯ assert(a == -1);
+        return element;
+    }
+private:
+    //int size;
+    long y;
+    long x;  
+    T element;
+    Matrix<T, N>* matrixPtr;
+};
